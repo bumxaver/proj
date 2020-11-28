ab99adda359b05aac33f4e794a77f687fe6acf2a
sadmint <bumxaver@mail.ru>
2020-11-28 12:13:06 +0300

    improve

--- install/install.sh
+++ install/install.sh
@@ -3,8 +3,18 @@
 cp ./fonts.conf ~/.fonts.conf
 cp ./gtkrc-2.0 ~/.gtkrc-2.0
 
+#install lovely fonts
 sudo cp ./fonts/JetBrainsMono-*  /usr/share/fonts && sudo cp ./fonts/Verdana.* /usr/share/fonts
+fc-cache -f -v 
+sleep 2
+
+#add i3 conf && theme
 cp config ~/.config/i3/
 cp -r gtk-3.0 ~/.config
 cp -r gtk-4.0 ~/.config
 sudo cp qt-hidpi.sh /etc/profile.d
+
+#add layout
+setxkbmap "us,ru" ",winkeys" "grp:win_space_toggle,grp_led:scroll"
+sleep 2
+sudo -u ${USER} localectl set-x11-keymap us,ru pc104 "" grp:win_space_toggle
--- install/qt-hidpi.sh
+++ install/qt-hidpi.sh
@@ -2,4 +2,3 @@
 export QT_SCALE_FACTOR=1.2
 export QT_AUTO_SCREEN_SCALE_FACTOR=1.2
 export GTK_THEME=Adwaita-dark
-export GDK_DPI_SCALE=1.3
